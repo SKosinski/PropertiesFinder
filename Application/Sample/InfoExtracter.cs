dc5e9b62e5c47271862ddbfee4e554d19e250266
Stanisław Kosiński <33428124+SKosinski@users.noreply.github.com>
2020-04-12 20:51:31 +0200

    Created Info Extracter
    methods implemented:
    Extract top bar
    Extract left column

---
+++ Application/Sample/InfoExtracter.cs
@@ -0,0 +1,101 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Sample
+{
+    class InfoExtracter
+    {
+        public static void ExtractInfoFromPropertyPage(Dictionary<string, string> info, HtmlDocument doc)
+        {
+            ExtractTopBar(info, doc);
+            ExtractLeftColumn(info, doc);
+            ExtractDescription(info, doc);
+        }
+
+        private static void ExtractDescription(Dictionary<string, string> info, HtmlDocument doc)
+        {
+            var dataInfo = doc.DocumentNode.SelectNodes("//div[@class=\"popis\"]");
+
+            var listInfo = new List<string>();
+            if (dataInfo != null)
+            {
+                ExtractInfo(dataInfo, listInfo);
+            }
+
+        }
+
+        private static void ExtractTopBar(Dictionary<string, string> info, HtmlDocument doc)
+        {
+            var dataInfo = doc.DocumentNode.SelectNodes("//span[@class=\"velikost10\"]");
+
+            var listInfo = new List<string>();
+            if (dataInfo != null)
+            {
+                ExtractInfo(dataInfo, listInfo);
+            }
+
+
+            string creationDate = listInfo[2];
+            char[] separators = { '[', '.', ' ', ']' };
+            var creationStrings = creationDate.Split(separators);
+            creationDate = creationStrings[3] + "-" + creationStrings[4] + "-" + creationStrings[6];
+            info.Add("CreationDateTime", creationDate);
+
+            var rawDescriptionInfo = doc.DocumentNode.SelectNodes("//h1[@class=\"nadpis\"]");
+            listInfo = new List<string>();
+            if (rawDescriptionInfo != null)
+            {
+                ExtractInfo(rawDescriptionInfo, listInfo);
+            }
+            info.Add("RawDescription", listInfo[0]);
+        }
+
+        private static void ExtractLeftColumn(Dictionary<string, string> info, HtmlDocument doc)
+        {
+            var contactInfo = doc.DocumentNode.SelectNodes("//td[@class=\"listadvlevo\"]");
+            var listInfo = new List<string>();
+            if (contactInfo != null)
+            {
+                ExtractInfo(contactInfo, listInfo);
+            }
+
+            for (int i = 0; i < listInfo.Count;)
+            {
+                if (listInfo[i].Contains("Imię:") || listInfo[i].Contains("Telefon:") || listInfo[i].Contains("Lokalizacja:") || listInfo[i].Contains("Widziało:") || listInfo[i].Contains("Cena:"))
+                {
+                    listInfo.Remove(listInfo[i]);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            info.Add("Name", listInfo[0]);
+            info.Add("Telephone", listInfo[1]);
+            var split = listInfo[3].Split(" ");
+            info.Add("DetailedAddress", split[0]);
+            info.Add("City", split[1]);
+            info.Add("TotalGrossPrice", listInfo[4]);
+        }
+
+        private static void ExtractInfo(HtmlNodeCollection nodeCol, List<string> info)
+        {
+            foreach (HtmlNode node in nodeCol)
+            {
+                if (node.ChildNodes.Count != 0)
+                {
+                    var newNodeCol = node.ChildNodes;
+                    ExtractInfo(newNodeCol, info);
+                }
+                else
+                {
+                    if (node.InnerText != "\r\n" && node.InnerText != "")
+                        info.Add(node.InnerText);
+                }
+            }
+        }
+    }
+}
+
--- Application/Sample/SampleIntegration.cs
+++ Application/Sample/SampleIntegration.cs
@@ -39,35 +39,6 @@ namespace Application.Sample
             HtmlWeb web = new HtmlWeb();
             HtmlDocument doc = web.Load(WebPage.Url);
             GetAllPropertyPages(pages, doc);
-
-            //foreach (var item in info)
-            //{
-            //    if(item.Contains("Imię:") || item.Contains("Telefon:") || item.Contains("Lokalizacja:") || item.Contains("Widziało:") || item.Contains("Cena:"))
-            //    {
-            //        info.Remove(item);
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine(item);
-            //    }
-            //}
-            //var bnode = node.Decendants().Select
-            //var anode = bnode.SelectNodes("//b").First();
-            //Console.WriteLine(anode.InnerText);
-
-            //var name = node.SelectNodes("//b").First().InnerText;
-            //var nodes = doc.DocumentNode.ChildNodes;
-            //foreach(HtmlAgilityPack.HtmlNode node in nodes)
-            //{
-            //    var infoTable = node.SelectSingleNode("//td[@class=\"listadvlevo\"]");
-            //    var bnode = infoTable.SelectSingleNode("./b");
-            //    var anode = bnode.SelectSingleNode("./a");
-            //    Console.WriteLine(anode.InnerText);
-            //}
-
-
-            var random = new Random();
-            var randomValue = random.Next() % 10;
             //Tutaj w normalnej sytuacji musimy ściągnąć dane z konkretnej strony, przeparsować je i dopiero wtedy zapisać do modelu Dump
 
             var dump = new Dump()
@@ -81,64 +52,57 @@ namespace Application.Sample
 
             foreach (var page in pages)
             {
-                List<string> info = new List<string>();
+                Dictionary<string, string> info = new Dictionary<string, string>();
                 doc = web.Load(page);
-                ExtractInfoFromPropertyPage(info, doc);
+                InfoExtracter.ExtractInfoFromPropertyPage(info, doc);
 
                 Entry entry = new Entry
                 {
-                    OfferDetails = new OfferDetails
-                    {
-                        Url = page,
-                        CreationDateTime = DateTime.Now,
-                        OfferKind = OfferKind.SALE,
-                        SellerContact = new SellerContact
-                        {
-                            Name = info[0]
-                        },
-                        IsStillValid = true
-                    },
-                    RawDescription = "Kup Teraz!"
+
+                    //OfferDetails = new OfferDetails
+                    //{
+                    //    Url = page,
+                    //    CreationDateTime = DateTime.Now,
+                    //    LastUpdateDateTime = null,
+                    //    OfferKind = OfferKind.RENTAL,
+                    //    SellerContact = new SellerContact
+                    //    {
+                    //        Email = null,
+                    //        Name = info[0],
+                    //        Telephone = info[1]
+                    //    },
+
+                    //    IsStillValid = true
+                    //},
+                    //PropertyFeatures = new PropertyFeatures
+                    //{
+                    //    GardenArea = null,
+                    //    Balconies = null,
+                    //    BasementArea = null,
+                    //    OutdoorParkingPlaces = null,
+                    //    IndoorParkingPlaces = null,
+                    //},
+                    //PropertyAddress = new PropertyAddress
+                    //{
+                    //    City = null,
+                    //    District = null,
+                    //    StreetName = null,
+                    //    DetailedAddress = null,
+                    //},
+                    //PropertyPrice = new PropertyPrice
+                    //{
+                    //    TotalGrossPrice = null,
+                    //    PricePerMeter = null,
+                    //    ResidentalRent = null,
+                    //},
+
+                    //RawDescription = "Kup Teraz!"
                 };
                 dumpEntries.Add(entry);
             }
             dump.Entries = dumpEntries;
 
             return dump;
-
-            //{
-            //    new Entry
-            //    {
-            //        OfferDetails = new OfferDetails
-            //        {
-            //            Url = $"{WebPage.Url}/{randomValue}",
-            //            CreationDateTime = DateTime.Now,
-            //            OfferKind = OfferKind.SALE,
-            //            SellerContact = new SellerContact
-            //            {
-            //                Email = "okazje@mieszkania.pl"
-            //            },
-            //            IsStillValid = true
-            //        },
-            //        RawDescription = "Kup Teraz!",
-            //    },
-            //    new Entry
-            //    {
-            //        OfferDetails = new OfferDetails
-            //        {
-            //            Url = $"{WebPage.Url}/{(randomValue+1)%10}",
-            //            CreationDateTime = DateTime.Now,
-            //            OfferKind = OfferKind.RENTAL,
-            //            SellerContact = new SellerContact
-            //            {
-            //                Email = "przeceny@mieszkania.pl"
-            //            },
-            //            IsStillValid = true
-            //        },
-            //        RawDescription = "NAPRAWDĘ WARTO!!",
-            //    }
-            //}
-
 
         }
 
@@ -152,44 +116,64 @@ namespace Application.Sample
                 pages.Add(address);
             }
         }
-
-        private static void ExtractInfoFromPropertyPage(List<string> info, HtmlDocument doc)
-        {
-            var contactInfo = doc.DocumentNode.SelectNodes("//td[@class=\"listadvlevo\"]");
-            
-            if (contactInfo != null)
-            {
-                ExtractInfo(contactInfo, info);
-            }
-
-            for (int i = 0; i < info.Count;)
-            {
-                if (info[i].Contains("Imię:") || info[i].Contains("Telefon:") || info[i].Contains("Lokalizacja:") || info[i].Contains("Widziało:") || info[i].Contains("Cena:"))
-                {
-                    info.Remove(info[i]);
-                }
-                else
-                {
-                    i++;
-                }
-            }
-        }
-
-        private static void ExtractInfo(HtmlNodeCollection nodeCol, List<string> info)
-        {
-            foreach (HtmlNode node in nodeCol)
-            {
-                if (node.ChildNodes.Count != 0)
-                {
-                    var newNodeCol = node.ChildNodes;
-                    ExtractInfo(newNodeCol, info);
-                }
-                else
-                {
-                    if (node.InnerText != "\r\n" && node.InnerText != "")
-                        info.Add(node.InnerText);
-                }
-            }
-        }
     }
 }
+
+//foreach (var item in info)
+//{
+//    if(item.Contains("Imię:") || item.Contains("Telefon:") || item.Contains("Lokalizacja:") || item.Contains("Widziało:") || item.Contains("Cena:"))
+//    {
+//        info.Remove(item);
+//    }
+//    else
+//    {
+//        Console.WriteLine(item);
+//    }
+//}
+//var bnode = node.Decendants().Select
+//var anode = bnode.SelectNodes("//b").First();
+//Console.WriteLine(anode.InnerText);
+
+//var name = node.SelectNodes("//b").First().InnerText;
+//var nodes = doc.DocumentNode.ChildNodes;
+//foreach(HtmlAgilityPack.HtmlNode node in nodes)
+//{
+//    var infoTable = node.SelectSingleNode("//td[@class=\"listadvlevo\"]");
+//    var bnode = infoTable.SelectSingleNode("./b");
+//    var anode = bnode.SelectSingleNode("./a");
+//    Console.WriteLine(anode.InnerText);
+//}
+
+//{
+//    new Entry
+//    {
+//        OfferDetails = new OfferDetails
+//        {
+//            Url = $"{WebPage.Url}/{randomValue}",
+//            CreationDateTime = DateTime.Now,
+//            OfferKind = OfferKind.SALE,
+//            SellerContact = new SellerContact
+//            {
+//                Email = "okazje@mieszkania.pl"
+//            },
+//            IsStillValid = true
+//        },
+//        RawDescription = "Kup Teraz!",
+//    },
+//    new Entry
+//    {
+//        OfferDetails = new OfferDetails
+//        {
+//            Url = $"{WebPage.Url}/{(randomValue+1)%10}",
+//            CreationDateTime = DateTime.Now,
+//            OfferKind = OfferKind.RENTAL,
+//            SellerContact = new SellerContact
+//            {
+//                Email = "przeceny@mieszkania.pl"
+//            },
+//            IsStillValid = true
+//        },
+//        RawDescription = "NAPRAWDĘ WARTO!!",
+//    }
+//}
+
